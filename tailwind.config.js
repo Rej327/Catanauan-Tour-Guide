4d39e274b2982d50ee297cc78fdb1b71a5f3d7db
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-07 11:40:55 +0800

    Additional Local Font Family and setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ export default {
     "./components/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ["Mistrully", "sans-serif"],
+        head: ["CocoGothic-Bold_trial", "sans-serif"],
+        body: ["CocoGothic_trial", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
