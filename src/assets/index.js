4b638f77e093981e106829f2ca1af2d44622e8b9
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-08 18:10:25 +0800

    Update in Navigation

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,2 @@
 export { default as hero } from "./HeroSection/hero.jpg";
+export { default as logo } from "./logo.png";
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Sample from "../components/Sample";
+import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
     <div>
+      <Navigation />
       <Hero />
       <Sample />
     </div>
