042a86250bf27ef9d9c5ddb1e66106859794ef44
Rej3277 <resujeff27@gmail.com>
2023-07-09 14:18:40 +0800

    Remove non use file

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Hero from "../components/Hero";
+import Hero from "../components/home_section/Hero";
 import Sample from "../components/Sample";
-import Navigation from "../components/Navigation";
+import Navigation from "../components/common/Navigation";
+import PlanAhead from "../components/home_section/PlanAhead";
 
 const Home = () => {
   return (
     <div>
       <Navigation />
       <Hero />
+      <PlanAhead />
       <Sample />
     </div>
   );
