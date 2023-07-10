8c149c1e3999b2c122106c3dbcb9429ccf72d5aa
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-10 14:48:50 +0800

    Step Into Summer Section

--- src/components/home_section/SetupInto.jsx
+++ src/components/home_section/SetupInto.jsx
@@ -4,21 +4,23 @@ import Button from "../common/Button";
 const SetupInto = ({ className }) => {
   return (
     <div className={`w-full ${className}`}>
-      <div className="bg-[url(./assets/StepInto/stepinto.jpg)] bg-fixed bg-repeat-round h-[450px]">
+      <div className="bg-[url(./assets/StepInto/stepinto.jpg)] bg-fixed bg-cover  h-[450px]">
         <div className="bg-black bg-opacity-50 w-full h-[450px] flex items-center flex-col justify-center">
-          <div className="w-fit ml-[50%] text-right max-w-[500px]">
-            <p className="text-white text-4xl tracking-widest font-bold">
-              STEP INTO SUMMER
-            </p>
-            <p className="text-white text-lg my-5">
-              With sunnier days and longer evenings. It's time to embrace the
-              great outdoors. Start your summer off right with our guide to
-              whats on around Catanauan.
-            </p>
-            <Button
-              label="What's On"
-              className="text-[#F1BE66] font-bold hover:text-black pt-4 pb-2 px-4 tracking-wider"
-            />
+          <div className="max-w-[1300px] px-2">
+            <div className="w-fit max-md:mx-2 max-lg:mx-10 mx-auto text-center md:px-0 lg:ml-[50%] md:text-right">
+              <p className="w-fit max-sm:mx-auto ml-auto text-white text-2xl md:text-4xl tracking-widest font-bold">
+                STEP INTO SUMMER
+              </p>
+              <p className="text-white text-lg my-5 max-w-[1500px]">
+                With sunnier days and longer evenings. It's time to embrace the
+                great outdoors. Start your summer off right with our guide to
+                whats on around Catanauan.
+              </p>
+              <Button
+                label="What's On"
+                className="text-[#F1BE66] font-bold hover:text-black pt-4 pb-2 px-4 tracking-wider"
+              />
+            </div>
           </div>
         </div>
       </div>
