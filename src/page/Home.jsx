62d9a1def0d2a1b60a29ef00a412dc1654991901
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-11 16:18:06 +0800

    Add resorts

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -5,6 +5,8 @@ import Navigation from "../components/common/Navigation";
 import PlanAhead from "../components/home_section/PlanAhead";
 import Adventure from "../components/home_section/Adventure";
 import SetupInto from "../components/home_section/SetupInto";
+import Carousel from "../components/common/Carousel";
+import Resorts from "../components/home_section/Resorts";
 
 const Home = () => {
   return (
@@ -14,6 +16,8 @@ const Home = () => {
       <PlanAhead className="py-6 md:py-12 md:mt-12 px-2" />
       <Adventure className="py-6 md:py-12 px-2" />
       <SetupInto className="py-6 md:py-12" />
+      <Resorts className="py-6 md:py-12" />
+      {/* <Carousel /> */}
       {/* <Sample /> */}
     </div>
   );
