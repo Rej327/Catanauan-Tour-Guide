0002e62b3917c354c29b2caf000f4b14eb52b583
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-12 16:03:30 +0800

    Add Hotel And Resto Section

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,3 +9,7 @@ export { default as adv } from "./AdventureSection/adv.jpg";
 export { default as adv1 } from "./AdventureSection/adv1.jpg";
 export { default as stepinto } from "./StepInto/stepinto.jpg";
 export { default as hotelandresto } from "./HotelAndResto/hotelandresto.jpg";
+export { default as hc1 } from "./HotelAndRestoCarouselItem/hc1.jpg";
+export { default as hc2 } from "./HotelAndRestoCarouselItem/hc2.jpg";
+export { default as rc1 } from "./HotelAndRestoCarouselItem/rc1.jpg";
+export { default as rc2 } from "./HotelAndRestoCarouselItem/rc2.jpg";
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -5,8 +5,9 @@ import Navigation from "../components/common/Navigation";
 import PlanAhead from "../components/home_section/PlanAhead";
 import Adventure from "../components/home_section/Adventure";
 import SetupInto from "../components/home_section/SetupInto";
-import Carousel from "../components/common/Carousel";
 import Resorts from "../components/home_section/Resorts";
+import HotelAndResto from "../components/home_section/HotelAndResto";
+import HotelAndRestoCarouselItem from "../components/home_section/HotelAndRestoCarouselItem";
 
 const Home = () => {
   return (
@@ -17,7 +18,9 @@ const Home = () => {
       <Adventure className="py-6 md:py-12 px-2" />
       <SetupInto className="py-6 md:py-12" />
       <Resorts className="py-6 md:py-12" />
-      {/* <Carousel /> */}
+      <HotelAndResto className="max-sm:mt-24 md:mt-12" />
+      {/* <HotelAndRestoCarouselItem className="max-sm:mt-24 md:mt-12" /> */}
+
       {/* <Sample /> */}
     </div>
   );
