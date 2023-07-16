bf673f4cbaba04f150173f033455a56754cfa2d9
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-16 11:38:58 +0800

    Add Cover In Resort Page

---
+++ src/components/resorts_page/Cover.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ArrowDown from "../common/ArrowDown";
+
+const Cover = () => {
+  const handleScroll = () => {
+    window.scrollTo({
+      top: window.innerHeight,
+    });
+  };
+
+  return (
+    <div className="w-full">
+      <div className="bg-[url(./assets/ResortsPage/resortcover.jpg)] bg-cover bg-center h-screen">
+        <div className="relative bg-black bg-opacity-60 w-full h-screen flex items-center flex-col justify-center ">
+          <p className="text-4xl lg:text-5xl text-white font-title text-center tracking-wider">
+            Place to relax and unwind
+          </p>
+          <ArrowDown className="absolute bottom-0" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cover;
--- src/page/ResortPage.jsx
+++ src/page/ResortPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Footer from "../components/common/Footer";
 import NavigatonInOtherPage from "../components/common/NavigatonInOtherPage";
+import Cover from "../components/resorts_page/Cover";
 
 const ResortPage = () => {
   return (
     <div>
       <NavigatonInOtherPage />
+      <div>
+        <Cover />
+      </div>
       <Footer />
     </div>
   );
