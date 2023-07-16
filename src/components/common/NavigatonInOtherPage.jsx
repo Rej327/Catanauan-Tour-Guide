33f9737d3bba45c59c162973dfa19a1f497cf348
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-16 11:22:12 +0800

    Modify margin bottom

--- src/components/common/NavigatonInOtherPage.jsx
+++ src/components/common/NavigatonInOtherPage.jsx
@@ -6,7 +6,7 @@ import SearchBar from "./SearchBar";
 const NavigatonInOtherPage = () => {
   return (
     <div>
-      <div className="w-full bg-[#FCE9CB] rounded-b-2xl pb-2 mb-10">
+      <div className="w-full bg-[#FCE9CB] rounded-b-2xl pb-2 mb-2">
         <div className="w-full bg-[#F9DBA9]  rounded-b-2xl">
           <div className="max-w-[1300px] mx-auto px-2 flex justify-between items-center py-7 lg:py-14">
             <p className="text-3xl font-header">DiscoverCatanauan</p>
