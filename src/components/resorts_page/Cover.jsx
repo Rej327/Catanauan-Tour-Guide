ecdc892d5707fdb2767c1050a28b138e548a29d1
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-16 11:43:21 +0800

    Change to paralax design

--- src/components/resorts_page/Cover.jsx
+++ src/components/resorts_page/Cover.jsx
@@ -4,7 +4,7 @@ import ArrowDown from "../common/ArrowDown";
 const Cover = ({ className }) => {
   return (
     <div className={`w-full ${className}`}>
-      <div className="bg-[url(./assets/ResortsPage/resortcover.jpg)] bg-cover bg-center h-screen">
+      <div className="bg-[url(./assets/ResortsPage/resortcover.jpg)] bg-fixed bg-cover bg-center h-screen">
         <div className="relative bg-black bg-opacity-60 w-full h-screen flex items-center flex-col justify-center ">
           <p className="text-4xl lg:text-5xl text-white font-title text-center tracking-wider">
             Place to relax and unwind
