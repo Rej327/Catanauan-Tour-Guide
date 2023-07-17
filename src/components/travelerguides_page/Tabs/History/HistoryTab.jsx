37c12824fb4c8d13f55b0440ad31230be7cd1d40
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-17 12:48:47 +0800

    Tabs Section For History Tab

---
+++ src/components/travelerguides_page/Tabs/History/HistoryTab.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  Tab,
+  TabIndicator,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import Content from "./Content";
+
+const HistoryTab = () => {
+  return (
+    <div className="w-full">
+      <Tabs position="relative" variant="unstyled">
+        <TabList gap="1rem">
+          <Tab>
+            <p className="font-bold">History</p>
+          </Tab>
+          <Tab>
+            <p className="font-bold">Events</p>
+          </Tab>
+          <Tab>
+            <p className="font-bold">Locals</p>
+          </Tab>
+        </TabList>
+        <TabIndicator
+          mt="2.5px"
+          height="3px"
+          bg="orange.200"
+          borderRadius="1px"
+        />
+        <TabPanels>
+          <TabPanel>
+            {/* Tab 1 */}
+            <Content />
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
+};
+
+export default HistoryTab;
