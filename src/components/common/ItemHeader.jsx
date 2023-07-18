a96fee111d250c31d888ad5c3f30e85eaaefe509
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-18 16:16:51 +0800

    Reusable Item Header for Item Components

---
+++ src/components/common/ItemHeader.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ItemHeader = ({ className, selectedPage, textStyle }) => {
+  return (
+    <div
+      className={`max-w-[1300px] mx-auto flex flex-col justify-center ${className}`}
+    >
+      <div className="flex gap-2 my-10 text-sm">
+        <Link to="/">
+          <p className="hover:font-bold hover:underline underline-offset-8 cursor-pointer">
+            Home
+          </p>
+        </Link>
+        <p> | </p>
+        <p className={`font-bold underline underline-offset-8 ${textStyle}`}>
+          {selectedPage}
+        </p>
+      </div>
+      <div className="mb-10">
+        <p className={`font-bold text-3xl md:text-4xl ${textStyle}`}>
+          {selectedPage}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ItemHeader;
