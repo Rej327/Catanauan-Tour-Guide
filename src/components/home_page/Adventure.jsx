1f906a057d462ebdecc22fdeb0a4a5d5154ae68b
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-19 14:42:58 +0800

    Replace div color to shadow

--- src/components/home_page/Adventure.jsx
+++ src/components/home_page/Adventure.jsx
@@ -47,10 +47,9 @@ const Adventure = ({ className }) => {
             </div>
           </div>
 
-          {/* DESKTOP SIZE */}
-          <div className="relative w-[270px] h-[270px] mx-auto md:hidden">
-            <div className="absolute bottom-0 right-0 bg-[#F8DBA8] w-[300px] h-[300px]"></div>
-            <div className=" absolute bg-[url(./assets/AdventureSection/adv1.jpg)] w-[300px] h-[300px] bg-cover bg-center">
+          {/* PHONE SIZE */}
+          <div className="w-[300px] h-[300px] mx-auto md:hidden">
+            <div className="mx-auto absolute bg-[url(./assets/AdventureSection/adv1.jpg)] w-[300px] h-[300px] bg-cover bg-center advShadow">
               <div className="relative bg-black bg-opacity-60 w-[300px] h-[300px] flex items-center justify-center">
                 <p className="w-fit text-white font-header text-3xl text-center ">
                   # Keep <br /> Discovering
@@ -59,11 +58,10 @@ const Adventure = ({ className }) => {
             </div>
           </div>
 
-          {/* PHONE SIZE */}
+          {/* DESKTOP SIZE */}
           <div className="relative my-5">
-            <div className="relative w-[530px] h-[530px] mx-auto max-sm:hidden">
-              <div className="absolute bottom-0 right-0 bg-[#F8DBA8] w-[500px] h-[500px]"></div>
-              <div className=" absolute bg-[url(./assets/AdventureSection/adv1.jpg)] w-[500px] h-[500px] bg-cover bg-center">
+            <div className="relative w-[500px] h-[500px] mx-auto max-sm:hidden">
+              <div className="mx-auto absolute bg-[url(./assets/AdventureSection/adv1.jpg)] w-[500px] h-[500px] bg-cover bg-center advShadow">
                 <div className="relative bg-black bg-opacity-60 w-[500px] h-[500px]">
                   <p className="absolute bottom-24 right-12 w-fit text-white font-header text-6xl leading-[5rem] -rotate-45">
                     # Keep <br /> Discovering
