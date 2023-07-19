fe8f57211cdfbdd5d856550f8f18f48714aaf3bd
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-19 14:11:33 +0800

    Rewind the code

--- src/components/common/ItemHeader.jsx
+++ src/components/common/ItemHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ItemHeader = ({ className, selectedPage, textStyle, titleContainer }) => {
+const ItemHeader = ({ className, selectedPage, textStyle }) => {
   return (
     <div
       className={`max-w-[1300px] px-2 mx-auto flex flex-col justify-center ${className}`}
@@ -17,7 +17,7 @@ const ItemHeader = ({ className, selectedPage, textStyle, titleContainer }) => {
           {selectedPage}
         </p>
       </div>
-      <div className={`mb-10 ${titleContainer}`}>
+      <div className="mb-10">
         <p className={`font-bold text-3xl md:text-4xl ${textStyle}`}>
           {selectedPage}
         </p>
