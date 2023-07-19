a024b960e371b6d6e376d173a212e9523572fbfe
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-19 15:45:15 +0800

    Add Event or non business header component

---
+++ src/components/common/EventHeaader.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const EventHeader = ({ className, selectedPage, textStyle, imgCover }) => {
+  return (
+    <div
+      className={`max-w-[1300px] px-2 mx-auto flex flex-col justify-center ${className}`}
+    >
+      <div className="flex gap-2 my-10 text-sm">
+        <Link to="/">
+          <p className="hover:font-bold hover:underline underline-offset-8 cursor-pointer">
+            Home
+          </p>
+        </Link>
+        <p> | </p>
+        <p className={`font-bold underline underline-offset-8 ${textStyle}`}>
+          {selectedPage}
+        </p>
+      </div>
+      <div className="mb-10 flex flex-wrap gap-4 lg:gap-[9rem]">
+        <p
+          className={`font-bold text-3xl md:text-5xl max-w-[500px] ${textStyle}`}
+        >
+          {selectedPage}
+        </p>
+        <div className="max-w-[600px] mx-auto lg:mx-0">
+          <img src={imgCover} className="w-full object-cover borderShadow" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventHeader;
