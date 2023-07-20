a0cf273c403d38474cc0afe61eb39637daec5f67
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-20 15:33:31 +0800

    Add Data for term of use page

--- src/translation/index.js
+++ src/translation/index.js
@@ -83,6 +83,67 @@ export const en = {
       },
     },
   ],
+
+  // LEGAL PAGE
+  legal: {
+    id: 1,
+    term_of_use: [
+      {
+        id: 1,
+        title: "Content and Purpose:",
+        content:
+          "Our website provides tour guide content and information for users interested in exploring various destinations. The content is intended for general informational purposes only and should not be considered as professional advice. We do not guarantee the accuracy, completeness, or timeliness of the information provided. Users are responsible for verifying the information with reliable sources before making any travel decisions.",
+      },
+      {
+        id: 2,
+        title: "Use of Website:",
+        content:
+          "Our website is open for all users, and no login or registration is required to access the content. By using our website, you agree to comply with all applicable laws, regulations, and these Terms of Use. You must not use the website for any unlawful or unauthorized purposes, and you are solely responsible for your actions while using our platform.",
+      },
+      {
+        id: 3,
+        title: "Intellectual Property:",
+        content:
+          "All content, materials, and information presented on the website, including text, images, graphics, logos, videos, and other media, are the property of our website or its content providers and are protected by intellectual property laws. You may not reproduce, distribute, modify, or otherwise use any of the content without obtaining prior written consent from the respective owners.",
+      },
+      {
+        id: 4,
+        title: "Third-Party Websites:",
+        content:
+          "Our website may contain links to third-party websites for additional information or resources. These links are provided for your convenience, and we do not endorse or assume any responsibility for the content or practices of these external sites. Visiting third-party websites through our links is at your own risk.",
+      },
+      {
+        id: 5,
+        title: "Limitation of Liability:",
+        content:
+          "In no event shall our website, its affiliates, partners, or contributors be liable for any direct, indirect, incidental, consequential, or special damages arising out of or in connection with your use of the website or reliance on its content. We do not guarantee the continuous availability of the website or that it will be free from errors, viruses, or other harmful components.",
+      },
+      {
+        id: 6,
+        title: "Indemnification:",
+        content:
+          "You agree to indemnify and hold harmless our website, its owners, employees, and agents from any claims, liabilities, damages, losses, or expenses, including attorneys' fees, arising out of your use of the website or violation of these Terms of Use.",
+      },
+      {
+        id: 7,
+        title: "Modifications:",
+        content:
+          "We reserve the right to update or modify these Terms of Use at any time without prior notice. It is your responsibility to review this page periodically for changes. Your continued use of the website after any modifications indicate your acceptance of the updated Terms of Use.",
+      },
+      {
+        id: 8,
+        title: "Governing Law and Jurisdiction:",
+        content:
+          "These Terms of Use shall be governed by and construed in accordance with the laws of Republic of the Philippines. Any dispute arising from or related to the use of our website shall be subject to the exclusive jurisdiction of the courts located in Republic of the Philippines.",
+      },
+      {
+        id: 9,
+        title: "Contact Us:",
+        content:
+          "If you have any questions, concerns, or feedback regarding these Terms of Use or our website, please contact us at [your email address].",
+      },
+    ],
+  },
 };
 
 //FILIPINO TRANSLATION
@@ -179,4 +240,65 @@ export const fil = {
       },
     },
   ],
+
+  // LEGAL PAGE
+  legal: {
+    id: 1,
+    term_of_use: [
+      {
+        id: 1,
+        title: "Nilalaman at Layunin:",
+        content:
+          "Ang aming website ay nagbibigay ng nilalaman at impormasyon para sa mga user na interesado sa paglalakbay sa iba't ibang destinasyon. Ang nilalaman ay inilaan lamang para sa pangkalahatang impormasyon at hindi dapat ituring bilang propesyonal na payo. Hindi namin maipapangako ang eksaktong katumpakan, kabuuan, o kasalukuyang impormasyon na ibinibigay. Ang mga user ay responsable sa pag-verify ng impormasyon sa mga mapagkakatiwalaang pinagmumulan bago gumawa ng anumang desisyon sa paglalakbay.",
+      },
+      {
+        id: 2,
+        title: "Paggamit ng Website:",
+        content:
+          "Ang aming website ay bukas para sa lahat ng mga user, at hindi kinakailangan ang login o rehistrasyon para ma-access ang nilalaman. Sa pamamagitan ng paggamit ng aming website, sumasang-ayon kayong sumunod sa lahat ng batas, regulasyon, at Mga Tuntunin ng Paggamit na ito. Bawal gamitin ang website para sa anumang ilegal o hindi awtorisadong layunin, at kayo lamang ang responsable sa inyong mga gawain habang ginagamit ang aming platform.",
+      },
+      {
+        id: 3,
+        title: "Intelehensiyang Pag-aari:",
+        content:
+          "Ang lahat ng nilalaman, materyales, at impormasyong ipinapakita sa website, kasama ang teksto, mga imahe, grapiko, logo, bidyo, at iba pang media, ay pag-aari ng aming website o mga nagmamay-ari ng nilalaman at protektado ng batas sa karapatan ng intelehensiyang pag-aari. Hindi maaaring kopyahin, ipamahagi, baguhin, o gamitin sa anumang paraan ang anumang bahagi ng nilalaman nang walang pahintulot na isinusulat mula sa mga may-ari nito.",
+      },
+      {
+        id: 4,
+        title: "Mga Website ng Ikatlong Partido:",
+        content:
+          "Maaaring maglaman ang aming website ng mga link patungo sa mga website ng ikatlong partido para sa karagdagang impormasyon o mapagkukunan. Ang mga link na ito ay ibinibigay para sa inyong kaginhawaan, at hindi kami nag-eendorso o nag-aasume ng anumang responsibilidad sa nilalaman o mga patakaran ng mga panlabas na site. Ang pagbisita sa mga website ng ikatlong partido sa pamamagitan ng aming mga link ay nasa inyong sariling panganib.",
+      },
+      {
+        id: 5,
+        title: "Limitasyon ng Pananagutan:",
+        content:
+          "Sa anumang pangyayari, hindi kami mananagot, pati na rin ang aming website, mga kasosyo, o mga nag-ambag ng nilalaman, para sa anumang direktang, hindi direkta, di-inaasahang, pasalungat na pinsala, o espesyal na pinsala na nagmumula sa inyong paggamit ng website o pagtitiwala sa nilalaman nito. Hindi namin maipapangako ang patuloy na pagiging available ng website o ang pagiging walang error, virus, o iba pang mapanganib na sangkap nito.",
+      },
+      {
+        id: 6,
+        title: "Pagbibigay-Katuwang:",
+        content:
+          "Sumasang-ayon kayong itanggol at italaga ang aming website, mga may-ari, kawani, at ahente mula sa anumang mga reklamo, pananagutan, pinsala, pagkawala, o gastusin, kabilang ang mga bayad ng abogado, na nagmumula sa inyong paggamit ng website o paglabag sa Mga Tuntunin ng Paggamit na ito.",
+      },
+      {
+        id: 7,
+        title: "Mga Pagbabago:",
+        content:
+          "Tinatanggap namin ang karapatan na i-update o baguhin ang Mga Tuntunin ng Paggamit na ito anumang oras nang walang paunang abiso. Responsibilidad ninyo na regular na suriin ang pahinang ito para sa mga pagbabago. Ang patuloy ninyong paggamit ng website matapos ang anumang pagbabago ay nangangahulugan ng inyong pagsang-ayon sa nabago Mga Tuntunin ng Paggamit.",
+      },
+      {
+        id: 8,
+        title: "Batas at Jurisdiksyon:",
+        content:
+          "Ang Mga Tuntunin ng Paggamit na ito ay pangangasiwaan at tatalakayin ayon sa mga batas ng Republika ng Pilipinas. Anumang pagtatalo na magmumula o may kinalaman sa paggamit ng aming website ay sakop ng eksklusibong hurisdiksyon ng mga hukuman na matatagpuan sa Republika ng Pilipinas.",
+      },
+      {
+        id: 9,
+        title: " Makipag-ugnayan sa Amin:",
+        content:
+          "Kung mayroon kayong mga katanungan, alalahanin, o puna tungkol sa Mga Tuntunin ng Paggamit na ito o sa aming website, mangyaring makipag-ugnayan sa amin sa [iyong email address].",
+      },
+    ],
+  },
 };
