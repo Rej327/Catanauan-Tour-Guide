f0c0646a222d426608147409c2d1cd847e88b3eb
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-20 15:34:32 +0800

    Add Term of Use Component

---
+++ src/components/legal_page/TermOfUseHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReusableHeader from "../common/ReusableHeader";
+import { legal } from "../../assets";
+
+const TermOfUsuHeader = () => {
+  return (
+    <div className="px-2">
+      <ReusableHeader selectedPage="Term of Use" icon={legal} />
+    </div>
+  );
+};
+
+export default TermOfUsuHeader;
