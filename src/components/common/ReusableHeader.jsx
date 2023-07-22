b9e0e6865753b5dc83162e7410ac357aaa0cd651
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-22 12:19:39 +0800

    Edit Reusable Header padding X

--- src/components/common/ReusableHeader.jsx
+++ src/components/common/ReusableHeader.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const ReusableHeader = ({ className, selectedPage, icon }) => {
   return (
     <div
-      className={`max-w-[1300px] mx-auto flex flex-col justify-center ${className}`}
+      className={`max-w-[1300px] px-4 mx-auto flex flex-col justify-center ${className}`}
     >
       <div className="flex gap-2 my-10 text-sm">
         <Link to="/">
