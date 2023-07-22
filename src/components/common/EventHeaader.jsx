9877a2ac9ac31b7d5a3540858afc13705e6223d3
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-22 12:19:29 +0800

    Edit Event Header padding X

--- src/components/common/EventHeaader.jsx
+++ src/components/common/EventHeaader.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const EventHeader = ({ className, selectedPage, textStyle, imgCover }) => {
   return (
     <div
-      className={`max-w-[1300px] px-2 mx-auto flex flex-col justify-center ${className}`}
+      className={`max-w-[1300px] px-4 mx-auto flex flex-col justify-center ${className}`}
     >
       <div className="flex gap-2 my-10 text-sm">
         <Link to="/">
