0b6ad47dfcc28175b789e6a0d84b1a94b4a43e5a
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-07-31 16:08:45 +0800

    Add Item in Category All in Resto Page

--- src/components/resto_page/Tabs/All.jsx
+++ src/components/resto_page/Tabs/All.jsx
@@ -1,89 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import { rc1 } from "../../../assets";
+import { LangContext } from "../../../App";
 
 const All = () => {
+  const { language } = useContext(LangContext);
+  console.log(language.restaurants_page.category_all);
   return (
     <div className="max-w-[1300px] my-10 mx-auto flex flex-wrap justify-center items-center gap-10">
-      {/* FIRST ITEM */}
-      <div className="relative restoItems w-fit h-fit">
-        <div className="absolute z-10 bottom-0 bg-black bg-opacity-50 flex flex-col justify-center items-center w-full h-[75px]">
-          <p className="text-lg text-[#fefeff]">Alfredos Restaurant</p>
-          <p className="text-[#f9dba9] text-base hover:underline underline-offset-8 hover:cursor-pointer">
-            View here
-          </p>
+      {language.restaurants_page.category_all.map((data) => (
+        <div
+          key={data.id}
+          className="rounded-2xl relative restoItems w-fit h-fit"
+        >
+          <div className="absolute rounded-b-2xl z-10 bottom-0 bg-black bg-opacity-50 flex flex-col justify-center items-center w-full h-[75px]">
+            <p className="text-lg text-[#fefeff]">{data.title}</p>
+            <p className="text-[#f9dba9] text-base hover:underline underline-offset-8 hover:cursor-pointer">
+              {data.btn_txt}
+            </p>
+          </div>
+          <div className="rounded-2xl w-[320px] h-[320px] overflow-hidden">
+            <img
+              src={data.img}
+              alt={`${data.title} Image`}
+              className="w-full h-full object-cover itemImg"
+            />
+          </div>
         </div>
-        <div className="max-w-[350px] h-[350px] overflow-hidden">
-          <img
-            src={rc1}
-            alt="Restaurant Image"
-            className="w-full h-full object-cover itemImg"
-          />
-        </div>
-      </div>
-      {/* NEXT ITEM */}
-      <div className="relative restoItems w-fit h-fit">
-        <div className="absolute z-10 bottom-0 bg-black bg-opacity-50 flex flex-col justify-center items-center w-full h-[75px]">
-          <p className="text-lg text-[#fefeff]">Alfredos Restaurant</p>
-          <p className="text-[#f9dba9] text-base hover:underline underline-offset-8 hover:cursor-pointer">
-            View here
-          </p>
-        </div>
-        <div className="max-w-[350px] h-[350px] overflow-hidden">
-          <img
-            src={rc1}
-            alt="Restaurant Image"
-            className="w-full h-full object-cover itemImg"
-          />
-        </div>
-      </div>
-      {/* NEXT ITEM */}
-      <div className="relative restoItems w-fit h-fit">
-        <div className="absolute z-10 bottom-0 bg-black bg-opacity-50 flex flex-col justify-center items-center w-full h-[75px]">
-          <p className="text-lg text-[#fefeff]">Alfredos Restaurant</p>
-          <p className="text-[#f9dba9] text-base hover:underline underline-offset-8 hover:cursor-pointer">
-            View here
-          </p>
-        </div>
-        <div className="max-w-[350px] h-[350px] overflow-hidden">
-          <img
-            src={rc1}
-            alt="Restaurant Image"
-            className="w-full h-full object-cover itemImg"
-          />
-        </div>
-      </div>
-      {/* NEXT ITEM */}
-      <div className="relative restoItems w-fit h-fit">
-        <div className="absolute z-10 bottom-0 bg-black bg-opacity-50 flex flex-col justify-center items-center w-full h-[75px]">
-          <p className="text-lg text-[#fefeff]">Alfredos Restaurant</p>
-          <p className="text-[#f9dba9] text-base hover:underline underline-offset-8 hover:cursor-pointer">
-            View here
-          </p>
-        </div>
-        <div className="max-w-[350px] h-[350px] overflow-hidden">
-          <img
-            src={rc1}
-            alt="Restaurant Image"
-            className="w-full h-full object-cover itemImg"
-          />
-        </div>
-      </div>
-      {/* NEXT ITEM */}
-      <div className="relative restoItems w-fit h-fit">
-        <div className="absolute z-10 bottom-0 bg-black bg-opacity-50 flex flex-col justify-center items-center w-full h-[75px]">
-          <p className="text-lg text-[#fefeff]">Alfredos Restaurant</p>
-          <p className="text-[#f9dba9] text-base hover:underline underline-offset-8 hover:cursor-pointer">
-            View here
-          </p>
-        </div>
-        <div className="max-w-[350px] h-[350px] overflow-hidden">
-          <img
-            src={rc1}
-            alt="Restaurant Image"
-            className="w-full h-full object-cover itemImg"
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
