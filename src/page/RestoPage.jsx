064127978de0e14217ae46faad0bab58e43c1415
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-01 11:18:07 +0800

    Add Scroll to top when navigate in this page

--- src/page/RestoPage.jsx
+++ src/page/RestoPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../components/common/Footer";
 import NavigatonInOtherPage from "../components/common/NavigatonInOtherPage";
 import RestoCover from "../components/resto_page/RestoCover";
@@ -6,6 +6,10 @@ import RestoBodyDesktop from "../components/resto_page/RestoBodyDesktop";
 import RestoBodyMobile from "../components/resto_page/RestoBodyMobile";
 
 const ResortPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <NavigatonInOtherPage />
