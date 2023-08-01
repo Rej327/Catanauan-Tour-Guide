9330085f0438ec09bb6aff305828f759473e1f1e
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-01 10:57:30 +0800

    Add data in transportation section in Home Page

--- src/components/home_page/Tranportation.jsx
+++ src/components/home_page/Tranportation.jsx
@@ -3,9 +3,14 @@ import { LangContext } from "../../App";
 import Carousel from "../common/Carousel";
 import Button from "../common/Button";
 import { Link } from "react-router-dom";
+import { ImLocation } from "react-icons/im";
+import { Tooltip } from "@chakra-ui/react";
+import { SiAlwaysdata } from "react-icons/si";
+import { PiHandCoinsDuotone } from "react-icons/pi";
+import { getDirection } from "../helper";
 
 const Transportations = ({ className, id }) => {
-  const { language } = useContext(LangContext);
+  const { language, startingPoint } = useContext(LangContext);
   return (
     <div id={id} className={`w-full ${className}`}>
       <div className="w-full flex">
@@ -57,7 +62,65 @@ const Transportations = ({ className, id }) => {
             </p>
           </div>
         </div>
-        <div className="mx-auto max-w-[450px] lg:max-w-[1300px] -mt-5 lg:mt-0">
+        <div className="max-w-[1300px] -mt-5 mx-auto justify-evenly items-center flex flex-wrap gap-10">
+          {language.transportations_page.slice(0, 3).map((data) => (
+            <div
+              key={data.id}
+              className="max-w-[320px] resortItems flex flex-col gap-2 bg-white rounded-xl"
+            >
+              <div className="rounded-xl w-full  overflow-hidden">
+                <img
+                  src={data.img}
+                  className="w-full max-h-[320px] itemImg object-cover rounded-xl"
+                />
+              </div>
+              <div className="w-full mt-2 mb-4 px-2 flex flex-col gap-2">
+                <p className="text-lg font-extrabold tracking-widest">
+                  {data.toda}
+                </p>
+                <div className="w-fit flex gap-1 items-center">
+                  <ImLocation className="text-[#f1be66]" />
+                  <Link to={data.loc_link} target="_blank">
+                    <Tooltip label="Location" placement="bottom">
+                      <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
+                        {data.loc}
+                      </p>
+                    </Tooltip>
+                  </Link>
+                </div>
+                <div className="w-fit flex gap-1 items-center">
+                  <SiAlwaysdata className="text-[#f1be66]" />
+                  <Tooltip label="Routes" placement="bottom">
+                    <Link to={data.rts_link} target="_blank">
+                      <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
+                        {data.rts}
+                      </p>
+                    </Link>
+                  </Tooltip>
+                </div>
+                <div className="w-fit flex gap-1 items-center">
+                  <PiHandCoinsDuotone className="text-[#f1be66]" />
+                  <Tooltip label="Fare" placement="bottom">
+                    <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
+                      ₱ {data.fare}
+                    </p>
+                  </Tooltip>
+                </div>
+                <div className="w-fit">
+                  <Link
+                    to={getDirection(data.direction, startingPoint)}
+                    target="_blank"
+                  >
+                    <p className="w-auto text-[#f1be66] hover:translate-x-5 duration-300 hover:font-bold">
+                      Get Direction &#8594;
+                    </p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* <div className="mx-auto max-w-[450px] lg:max-w-[1300px] -mt-5 lg:mt-0">
           <div className="mx-auto">
             <Carousel>
               {language?.main?.resorts.slice(0, 3).map((data) => (
@@ -77,7 +140,7 @@ const Transportations = ({ className, id }) => {
               ))}
             </Carousel>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
