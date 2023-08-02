2f0123e3d8c54194e39b535016545c3159ab95b9
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-02 12:21:05 +0800

    Add Link to Home Page on Logo

--- src/components/common/NavigatonInOtherPage.jsx
+++ src/components/common/NavigatonInOtherPage.jsx
@@ -10,7 +10,9 @@ const NavigatonInOtherPage = () => {
       <div className="w-full bg-[#FCE9CB] rounded-b-2xl pb-2 mb-2">
         <div className="w-full bg-[#F9DBA9]  rounded-b-2xl">
           <div className="max-w-[1300px] mx-auto px-2 flex justify-between items-center py-7 lg:py-14">
-            <p className="text-3xl font-header">DiscoverCatanauan</p>
+            <Link to="/">
+              <p className="text-3xl font-header">DiscoverCatanauan</p>
+            </Link>
             <SearchBar
               containerStyle="hidden lg:block w-[800px]"
               className="hidden lg:block w-[800px] bg-white focus:text-black placeholder:text-black"
