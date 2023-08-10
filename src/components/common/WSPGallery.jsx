e834739258a3e85df967a1e513ab8f8e37e36af5
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-10 12:03:17 +0800

    Testing (Adding Skeleton)

--- src/components/common/WSPGallery.jsx
+++ src/components/common/WSPGallery.jsx
@@ -1,41 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   BsFillArrowLeftCircleFill,
   BsFillArrowRightCircleFill,
 } from "react-icons/bs";
 import { IoIosClose } from "react-icons/io";
+import { Skeleton } from "@chakra-ui/react"; // Import the Skeleton component
 
 const WSPGallery = ({ setData }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [openModal, setOpenModal] = useState(false);
-  const [isLoading, setIsLoading] = useState(true); // Add loading state
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(loadingTimeout);
+  }, []); // Run only once when the component mounts
 
   const handleOpenModal = (index) => {
     setSlideNumber(index);
     setOpenModal(true);
   };
 
-  // Close Modal
   const handleCloseModal = () => {
     setOpenModal(false);
   };
 
-  // Previous Image
   const prevSlide = () => {
-    slideNumber === 0
-      ? setSlideNumber(setData?.length - 1)
-      : setSlideNumber(slideNumber - 1);
+    setSlideNumber((slideNumber + setData.length - 1) % setData.length);
   };
 
-  // Next Image
   const nextSlide = () => {
-    slideNumber + 1 === setData?.length
-      ? setSlideNumber(0)
-      : setSlideNumber(slideNumber + 1);
-  };
-
-  const handleImageLoad = () => {
-    setIsLoading(false);
+    setSlideNumber((slideNumber + 1) % setData.length);
   };
 
   return (
@@ -56,12 +54,12 @@ const WSPGallery = ({ setData }) => {
           />
           <div className="fullScreenImage">
             {isLoading ? (
-              <div className="loader center">Loading...</div>
+              <Skeleton isLoaded={!isLoading}></Skeleton>
             ) : (
               <img
                 src={setData?.[slideNumber].img}
                 alt=""
-                onLoad={handleImageLoad}
+                onLoad={() => setIsLoading(false)}
               />
             )}
           </div>
@@ -70,22 +68,21 @@ const WSPGallery = ({ setData }) => {
 
       <div className="flex flex-wrap gap-4 last:justify-normal justify-between items-center max-w-[1300px] mx-auto py-4 px-2">
         {setData &&
-          setData?.map((slide, index) => {
-            return (
-              <div
-                className="single overflow-hidden max-lg:mx-auto rounded-lg"
-                key={index}
-                onClick={() => handleOpenModal(index)}
-              >
+          setData?.map((slide, index) => (
+            <div
+              className="single overflow-hidden max-lg:mx-auto rounded-lg"
+              key={index}
+              onClick={() => handleOpenModal(index)}
+            >
+              <Skeleton isLoaded={!isLoading}>
                 <img
                   src={slide.img}
                   className="w-[300px] h-[290px] rounded-lg object-cover hover:scale-105 duration-300 cursor-zoom-in"
                   alt=""
-                  onLoad={handleImageLoad}
                 />
-              </div>
-            );
-          })}
+              </Skeleton>
+            </div>
+          ))}
       </div>
     </div>
   );
