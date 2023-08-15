22772839c38d22362228092aa5dfebbd5e000a55
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-15 14:45:03 +0800

    Add Zoom Contols in Img Gallery Selected Image

--- src/components/common/WSPGallery.jsx
+++ src/components/common/WSPGallery.jsx
@@ -3,31 +3,34 @@ import {
   BsFillArrowLeftCircleFill,
   BsFillArrowRightCircleFill,
 } from "react-icons/bs";
+import { AiOutlineZoomIn, AiOutlineZoomOut } from "react-icons/ai";
+import { BiReset } from "react-icons/bi";
 import { IoIosClose } from "react-icons/io";
 import { Skeleton } from "@chakra-ui/react"; // Import the Skeleton component
 
 const WSPGallery = ({ setData }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [openModal, setOpenModal] = useState(false);
+  const [isZoomed, setIsZoomed] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [zoomLevel, setZoomLevel] = useState(1);
+  const [panPosition, setPanPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const loadingTimeout = setTimeout(() => {
       setIsLoading(false);
     }, 999999);
 
-    // Create a reference to the image element
     const img = new Image();
     img.src = setData?.[slideNumber].img;
 
-    // Add an event listener for the 'load' event
     img.onload = () => {
       setIsLoading(false);
       clearTimeout(loadingTimeout);
     };
 
     return () => clearTimeout(loadingTimeout);
-  }, [setData, slideNumber]); // Include dependencies in the dependency array
+  }, [setData, slideNumber]);
 
   const handleOpenModal = (index) => {
     setSlideNumber(index);
@@ -46,6 +49,31 @@ const WSPGallery = ({ setData }) => {
     setSlideNumber((slideNumber + 1) % setData.length);
   };
 
+  const handleZoomIn = () => {
+    const newZoomLevel = Math.min(zoomLevel + 0.1, 2);
+    setPanPosition({ x: 0, y: 0 });
+    setZoomLevel(newZoomLevel);
+  };
+
+  const handleZoomOut = () => {
+    setZoomLevel(Math.max(zoomLevel - 0.1, 1));
+    setPanPosition({ x: 0, y: 0 });
+  };
+
+  const handleResetZoom = () => {
+    setZoomLevel(1);
+    setPanPosition({ x: 0, y: 0 });
+  };
+
+  const handlePan = (e) => {
+    if (!isZoomed) return;
+
+    setPanPosition({
+      x: panPosition.x + e.movementX / zoomLevel,
+      y: panPosition.y + e.movementY / zoomLevel,
+    });
+  };
+
   return (
     <div>
       {openModal && (
@@ -62,16 +90,49 @@ const WSPGallery = ({ setData }) => {
             className="btnNext text-2xl"
             onClick={nextSlide}
           />
-          <div className="fullScreenImage">
-            {isLoading ? (
-              <Skeleton isLoaded={!isLoading}></Skeleton>
-            ) : (
-              <img
-                src={setData?.[slideNumber].img}
-                alt=""
-                onLoad={() => setIsLoading(false)}
-              />
-            )}
+          <div
+            className="fullScreenImage"
+            style={{
+              cursor: isZoomed ? "grab" : "auto",
+              overflow: "hidden",
+            }}
+            onMouseMove={handlePan}
+            onMouseDown={() => setIsZoomed(true)}
+            onMouseUp={() => setIsZoomed(false)}
+          >
+            <div
+              className={`transition-transform duration-300 ${
+                isZoomed ? "scale-150" : "scale-100"
+              }`}
+              style={{
+                transform: `scale(${zoomLevel}) translate(${panPosition.x}px, ${panPosition.y}px)`,
+                transformOrigin: "center",
+              }}
+            >
+              {isLoading ? (
+                <Skeleton isLoaded={!isLoading}></Skeleton>
+              ) : (
+                <img
+                  src={setData?.[slideNumber].img}
+                  alt=""
+                  className="w-auto"
+                />
+              )}
+            </div>
+          </div>
+          <div className="zoomControls bg-black p-4 rounded-full bg-opacity-70 gap-10">
+            <AiOutlineZoomOut
+              className="text-2xl text-gray-300 cursor-pointer hover:text-white duration-300"
+              onClick={handleZoomOut}
+            />
+            <BiReset
+              className="text-2xl text-gray-300 cursor-pointer hover:text-white duration-300"
+              onClick={handleResetZoom}
+            />
+            <AiOutlineZoomIn
+              className="text-2xl text-gray-300 cursor-pointer hover:text-white duration-300"
+              onClick={handleZoomIn}
+            />
           </div>
         </div>
       )}
