cb3e36bc14795c774bdd38fe0616e06ef89bdc26
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-16 11:32:46 +0800

    Fetch Data (Festival Tabs)

--- src/components/planahead_item_page/culture_page/Tabs/Festival/FestivalContent.jsx
+++ src/components/planahead_item_page/culture_page/Tabs/Festival/FestivalContent.jsx
@@ -5,14 +5,21 @@ import { LangContext } from "../../../../../App";
 
 const FestivalContent = () => {
   const { language } = useContext(LangContext);
-  console.log(language.plan_page.cultures);
   return (
     <div className="w-full flex flex-wrap md:flex-nowrap gap-2 justify-center lg:justify-between ">
       <div className="max-w-[850px] flex flex-col gap-4 my-5">
         <p className="font-bold text-2xl">
-          {language?.plan_page?.cultures.title}
+          {language?.plan_page?.cultures?.festival?.title}
         </p>
-        <p className="text-lg">{language?.plan_page?.cultures.content}</p>
+        <p className="text-lg">
+          {language?.plan_page?.cultures?.festival?.content_0}
+        </p>
+        <p className="text-lg">
+          {language?.plan_page?.cultures?.festival?.content_1}
+        </p>
+        <p className="text-lg">
+          {language?.plan_page?.cultures?.festival?.content_2}
+        </p>
         <img
           src={festival_3}
           alt="Catanauan Image"
--- src/components/planahead_item_page/culture_page/Tabs/Festival/OtherContent.jsx
+++ src/components/planahead_item_page/culture_page/Tabs/Festival/OtherContent.jsx
@@ -1,25 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import { festival_0, festival_1, festival_2 } from "../../../../../assets";
+import { LangContext } from "../../../../../App";
 
 const OtherContent = () => {
+  const { language } = useContext(LangContext);
+  console.log(language?.cultures);
   return (
     <div className="w-full md:w-[300px] lg:w-[400px] bg-[#FCE9CB] flex flex-col gap-4 p-6 mx-auto">
       <p className="font-bold">Boling - Boling Festival Parade</p>
-      <img
-        src={festival_0}
-        alt="Boling-Boling Festival Image"
-        className="h-[300px] object-cover"
-      />
-      <img
-        src={festival_1}
-        alt="Boling-Boling Festival Image"
-        className="h-[300px] object-cover"
-      />
-      <img
-        src={festival_2}
-        alt="Boling-Boling Festival Image"
-        className="h-[300px] object-cover"
-      />
+      {language?.plan_page?.cultures?.festival?.festival_img
+        ?.slice(0, 3)
+        .map((data) => (
+          <div className="w-full">
+            <img
+              src={data.img}
+              alt="Boling-Boling Festival Image"
+              className="h-[300px] w-full object-cover"
+            />
+          </div>
+        ))}
     </div>
   );
 };
