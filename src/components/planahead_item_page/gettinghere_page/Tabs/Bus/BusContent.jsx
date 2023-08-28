f5c166a08d3330d710023d886c5fb56e98b6bed1
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-28 13:25:01 +0800

    Modify Code to fetch data from local storage

--- src/components/planahead_item_page/gettinghere_page/Tabs/Bus/BusContent.jsx
+++ src/components/planahead_item_page/gettinghere_page/Tabs/Bus/BusContent.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { bybus } from "../../../../../assets";
 import BusOtherContent from "./BusOtherContent";
+import { LangContext } from "../../../../../App";
 
 const BusContent = () => {
+  const { language } = useContext(LangContext);
+
   return (
     <div className="w-full flex flex-wrap md:flex-nowrap gap-2 justify-center lg:justify-between ">
-      <div className="max-w-[850px] flex flex-col gap-4 my-5">
-        <p className="font-bold text-2xl">By Bus</p>
+      <div className="max-w-[850px] flex flex-col gap-5 my-5">
+        <p className="font-bold text-2xl">
+          {" "}
+          {language?.plan_page?.getting_here?.by_bus?.title}
+        </p>
         <p className="text-lg">
-          The existence of Catanauan as a pueblo (town) was first recorded in
-          the map of Father Pedro Murillo dated on 1734. The recognized
-          foundation year of the Municipality was 1713 called as NABATASAN. In
-          the late 18th and 19th century, there were frequent Moro raids so the
-          people decided to construct two small stone forts or watch towers
-          (MAGKATANAWAN) mounted with artillery. Hence, the name CATANAUAN was
-          so derived.
+          {language?.plan_page?.getting_here?.by_bus?.content}
         </p>
         <img
           src={bybus}
           alt="Catanauan Image"
-          className="w-full h-[18rem] object-cover"
+          className="w-full h-[80vh] my-5 object-contain"
         />
+        <p className="font-bold text-2xl">
+          {" "}
+          {language?.plan_page?.getting_here?.by_bus?.sub_title_0}
+        </p>
+        <p className="text-lg">
+          {language?.plan_page?.getting_here?.by_bus?.sub_content_0}
+        </p>
+        <p className="font-bold text-2xl">
+          {" "}
+          {language?.plan_page?.getting_here?.by_bus?.sub_title_1}
+        </p>
+        <p className="text-lg">
+          {language?.plan_page?.getting_here?.by_bus?.sub_content_1}
+        </p>
       </div>
       <div className="w-[400px]">
         <BusOtherContent />
--- src/components/planahead_item_page/gettinghere_page/Tabs/Bus/BusOtherContent.jsx
+++ src/components/planahead_item_page/gettinghere_page/Tabs/Bus/BusOtherContent.jsx
@@ -1,15 +1,27 @@
-import React from "react";
-import { busstop } from "../../../../../assets";
+import React, { useContext } from "react";
+import { LangContext } from "../../../../../App";
+import { Link } from "react-router-dom";
 
 const BusOtherContent = () => {
+  const { language } = useContext(LangContext);
   return (
     <div className="w-full md:w-[300px] lg:w-[400px] bg-[#FCE9CB] flex flex-col gap-4 p-6 mx-auto">
-      <p className="font-bold text-lg">Lorem ipsum dolor sit amet.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      <img src={busstop} alt="History Image" />
-      <p className="font-bold text-lg">Lorem ipsum dolor sit amet.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-      <img src={busstop} alt="History Image" className="" />
+      <p className="font-bold text-lg">
+        {language?.plan_page?.getting_here?.by_bus?.other_title}
+      </p>
+      {language?.plan_page?.getting_here?.by_bus?.routes_links?.map((data) => (
+        <div>
+          <Link to={data.links} target="_blank">
+            <ul>
+              <li className="list-disc">
+                <p className="text-lg underline underline-offset-8 hover:no-underline">
+                  {data.routes}
+                </p>
+              </li>
+            </ul>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
