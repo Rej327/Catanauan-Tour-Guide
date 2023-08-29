3a946e49395e63b38939c0dc88d0019745ea5c1d
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-29 14:38:56 +0800

    Create Body Header for Getting Around Content

---
+++ src/components/planahead_item_page/gettingarround_page/BodyHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BodyHeader = () => {
+  return (
+    <div className="w-full bg-[#FEFEFF]">
+      <div className="max-w-[1300px] mx-auto">
+        <p className="text-3xl pt-[3rem]  opacity-90 font-extrabold">
+          — A JOURNEY THROUGH LOCAL FLAVORS
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BodyHeader;
