cc7d701ff14a68e5962f75269e8a6a465655f117
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-29 14:39:27 +0800

    Add BodyHeader and Delicasies Component

--- src/page/GettingAroundPage.jsx
+++ src/page/GettingAroundPage.jsx
@@ -4,6 +4,8 @@ import Footer from "../components/common/Footer";
 import GettingAroundHeader from "../components/planahead_item_page/gettingarround_page/GettingAroundHeader";
 import Market from "../components/planahead_item_page/gettingarround_page/Market";
 import GettingAroundSubHead from "../components/planahead_item_page/gettingarround_page/GettingAroundSubHead";
+import BodyHeader from "../components/planahead_item_page/gettingarround_page/BodyHeader";
+import Delicasies from "../components/planahead_item_page/gettingarround_page/Delicasies";
 
 const GettingAroundPage = () => {
   useEffect(() => {
@@ -16,7 +18,11 @@ const GettingAroundPage = () => {
       <div>
         <GettingAroundHeader />
         <GettingAroundSubHead />
-        <Market />
+        <div className="w-full bg-[#FEFEFF] ">
+          <BodyHeader />
+          <Delicasies />
+          <Market />
+        </div>
       </div>
       <Footer />
     </div>
