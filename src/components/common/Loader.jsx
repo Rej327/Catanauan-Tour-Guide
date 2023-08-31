d7a0de1d1b27483cb22a33a23114b54b5c00ecf1
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-31 12:12:04 +0800

    Add Loader Style

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -2,32 +2,9 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="loadingio-spinner-spin-jcs8iln04vb">
-      <div className="ldio-jd3x0i4hgvc">
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <div></div>
-        </div>
+    <div class="loadingio-spinner-eclipse-tyemwmmfkl">
+      <div class="ldio-1qiebrux8w7">
+        <div></div>
       </div>
     </div>
   );
