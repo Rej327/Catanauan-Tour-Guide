2f2988ef51f11441d78e4ee50ea3ca3476cfd31e
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-31 13:04:55 +0800

    Add Skeleton Component in Falls Section in Home Page

--- src/components/home_page/Falls.jsx
+++ src/components/home_page/Falls.jsx
@@ -1,13 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { LangContext } from "../../App";
 import Carousel from "../common/Carousel";
 import Button from "../common/Button";
 import { Link } from "react-router-dom";
-import { Tooltip } from "@chakra-ui/react";
+import { Skeleton, SkeletonText, Tooltip } from "@chakra-ui/react";
 import { ImLocation } from "react-icons/im";
 
 const Falls = ({ className, id }) => {
   const { language } = useContext(LangContext);
+  const [loading, setLoading] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+
+  const handleImageLoad = () => {
+    setImagesLoaded(imagesLoaded + 1);
+
+    if (imagesLoaded === language?.falls_page?.length) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 999999);
+
+    const imgPromises = language?.falls_page?.map((data) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = data.img;
+        img.onload = () => {
+          handleImageLoad();
+          resolve();
+        };
+      });
+    });
+
+    Promise.all(imgPromises).then(() => {
+      setLoading(false);
+      clearTimeout(loadingTimeout);
+    });
+
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
   return (
     <div id={id} className={`w-full ${className}`}>
       <div className="w-full flex">
@@ -54,26 +89,40 @@ const Falls = ({ className, id }) => {
               className="rounded-lg homeItemContainer max-w-[300px] md:max-w-[350px] lg:max-w-[350px] mx-auto"
             >
               <div className="overflow-hidden rounded-lg">
-                <img
-                  src={data.img}
-                  alt="resorts"
-                  className=" rounded-lg w-[350px] h-[300px] "
-                />
+                {loading ? (
+                  <Skeleton width="350px" height="300px" />
+                ) : (
+                  <img
+                    src={data.img}
+                    alt="resorts"
+                    className="rounded-lg w-[350px] h-[300px]"
+                    onLoad={handleImageLoad}
+                  />
+                )}
               </div>
               <div className="my-2">
-                <p className="text-lg font-bold pl-5">{data.title}</p>
-                <div className="w-fit flex gap-1 items-center">
-                  <p>
-                    <ImLocation className="text-[#f1be66]" />
-                  </p>
-                  <Link to={data.loc_link} target="_blank">
-                    <Tooltip label="Location" placement="bottom">
-                      <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
-                        {data.location}
+                {loading ? (
+                  <>
+                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                  </>
+                ) : (
+                  <>
+                    <p className="text-lg font-bold pl-5">{data.title}</p>
+                    <div className="w-fit flex gap-1 items-center">
+                      <p>
+                        <ImLocation className="text-[#f1be66]" />
                       </p>
-                    </Tooltip>
-                  </Link>
-                </div>
+                      <Link to={data.loc_link} target="_blank">
+                        <Tooltip label="Location" placement="bottom">
+                          <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
+                            {data.location}
+                          </p>
+                        </Tooltip>
+                      </Link>
+                    </div>
+                  </>
+                )}
               </div>
             </div>
           ))}
