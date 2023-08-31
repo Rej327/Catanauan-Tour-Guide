2969040f2132ecff00cf835f1dac1ee9739113ae
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-31 10:27:43 +0800

    Add Collapse Component in Food Hub Section

--- src/components/planahead_item_page/gettingarround_page/FoodHub.jsx
+++ src/components/planahead_item_page/gettingarround_page/FoodHub.jsx
@@ -1,5 +1,5 @@
-import { Tooltip } from "@chakra-ui/react";
-import React, { useContext } from "react";
+import { Collapse, Tooltip } from "@chakra-ui/react";
+import React, { useContext, useState } from "react";
 import { ImLocation } from "react-icons/im";
 import { Link } from "react-router-dom";
 import { getDirection } from "../../helper";
@@ -9,6 +9,8 @@ import PricesModal from "./PriceModal";
 
 const FoodHub = ({ className }) => {
   const { language, startingPoint } = useContext(LangContext);
+  const [show, setShow] = useState(false);
+  const handleToggle = () => setShow(!show);
 
   return (
     <div className={`w-full px-4 ${className}`}>
@@ -19,56 +21,72 @@ const FoodHub = ({ className }) => {
               <p className="text-2xl font-bold">Food Hub</p>
             </li>
           </ul>
-          <div className="w-full flex justify-between flex-wrap bg-[#FCE9CB gap-5 mx-auto">
-            {language?.plan_page?.getting_around?.content_item?.food_hub?.map(
-              (data) => (
-                <div className="bg-[#FEFEFF] rounded-lg homeItemContainer max-w-[300px] md:max-w-[350px] lg:max-w-[350px] mx-auto">
-                  <div className="overflow-hidden rounded-lg">
-                    <img
-                      src={data.img}
-                      alt="resorts"
-                      className=" rounded-lg w-[350px] h-[300px] "
-                    />
-                  </div>
-                  <div className="my-2 px-2">
-                    <p className="text-lg font-bold pl-5">{data.title}</p>
-                    <div className="w-fit my-2 flex gap-1 items-center">
-                      <p>
-                        <AiOutlineClockCircle className="text-[#f1be66]" />
-                      </p>
-                      <Tooltip label="Schedule" placement="bottom">
-                        <p className="text-sm">{data.schedule}</p>
-                      </Tooltip>
+
+          <Collapse
+            className="w-full flex justify-between flex-wrap bg-[#FCE9CB gap-5 mx-auto"
+            startingHeight={450}
+            in={show}
+          >
+            <div className="w-full flex justify-between flex-wrap bg-[#FCE9CB gap-5 mx-auto">
+              {language?.plan_page?.getting_around?.content_item?.food_hub?.map(
+                (data) => (
+                  <div className="bg-[#FEFEFF] rounded-lg homeItemContainer max-w-[300px] md:max-w-[350px] lg:max-w-[350px] mx-auto">
+                    <div className="overflow-hidden rounded-lg">
+                      <img
+                        src={data.img}
+                        alt="resorts"
+                        className=" rounded-lg w-[350px] h-[300px] "
+                      />
                     </div>
-                    <div className="w-fit my-2 flex gap-1 items-center">
-                      <p>
-                        <ImLocation className="text-[#f1be66]" />
-                      </p>
-                      <Link to={data.loc_link} target="_blank">
-                        <Tooltip label="Location" placement="bottom">
-                          <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
-                            {data.location}
-                          </p>
+                    <div className="my-2 px-2">
+                      <p className="text-lg font-bold pl-5">{data.title}</p>
+                      <div className="w-fit my-2 flex gap-1 items-center">
+                        <p>
+                          <AiOutlineClockCircle className="text-[#f1be66]" />
+                        </p>
+                        <Tooltip label="Schedule" placement="bottom">
+                          <p className="text-sm">{data.schedule}</p>
                         </Tooltip>
-                      </Link>
-                    </div>
-                    <div className="flex gap-2 justify-evenly items-center">
-                      {data?.menu?.item && (
-                        <PricesModal dataInfo={data?.menu?.item} />
-                      )}
-                      <p className="w-auto text-[#f1be66] text-center hover:text-[#be8624]">
-                        <Link
-                          to={getDirection(data.direction, startingPoint)}
-                          target="_blank"
-                        >
-                          Get Direction &#8594;
+                      </div>
+                      <div className="w-fit my-2 flex gap-1 items-center">
+                        <p>
+                          <ImLocation className="text-[#f1be66]" />
+                        </p>
+                        <Link to={data.loc_link} target="_blank">
+                          <Tooltip label="Location" placement="bottom">
+                            <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
+                              {data.location}
+                            </p>
+                          </Tooltip>
                         </Link>
-                      </p>
+                      </div>
+                      <div className="flex gap-2 justify-evenly items-center">
+                        {data?.menu?.item && (
+                          <PricesModal dataInfo={data?.menu?.item} />
+                        )}
+                        <p className="w-auto text-[#f1be66] text-center hover:text-[#be8624]">
+                          <Link
+                            to={getDirection(data.direction, startingPoint)}
+                            target="_blank"
+                          >
+                            Get Direction &#8594;
+                          </Link>
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )
-            )}
+                )
+              )}
+            </div>
+          </Collapse>
+
+          <div className="w-fit my-5 mx-auto">
+            <p
+              className="mx-auto w-fit text-black px-4 py-2 text-lg bg-[#FCE9CB] cursor-pointer rounded-full"
+              onClick={handleToggle}
+            >
+              {show ? "- Less" : "+ More"}
+            </p>
           </div>
         </div>
       </div>
