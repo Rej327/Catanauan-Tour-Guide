c7a4ebc77efef520589a7fd5e6a78b915d7ece59
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-08-31 13:30:45 +0800

    Add Skeleton Component in Transportation Section in Home Page

--- src/components/home_page/Tranportation.jsx
+++ src/components/home_page/Tranportation.jsx
@@ -1,16 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { LangContext } from "../../App";
 import Carousel from "../common/Carousel";
 import Button from "../common/Button";
 import { Link } from "react-router-dom";
 import { ImLocation } from "react-icons/im";
-import { Tooltip } from "@chakra-ui/react";
+import { Skeleton, SkeletonText, Tooltip } from "@chakra-ui/react";
 import { SiAlwaysdata } from "react-icons/si";
 import { PiHandCoinsDuotone } from "react-icons/pi";
 import { getDirection } from "../helper";
 
 const Transportations = ({ className, id }) => {
   const { language, startingPoint } = useContext(LangContext);
+  const [loading, setLoading] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+
+  const handleImageLoad = () => {
+    setImagesLoaded(imagesLoaded + 1);
+
+    if (imagesLoaded === language?.transportations_page?.length) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 999999);
+
+    const imgPromises = language?.transportations_page
+      .slice(0, 3)
+      ?.map((data) => {
+        return new Promise((resolve) => {
+          const img = new Image();
+          img.src = data.img;
+          img.onload = () => {
+            handleImageLoad();
+            resolve();
+          };
+        });
+      });
+
+    Promise.all(imgPromises).then(() => {
+      setLoading(false);
+      clearTimeout(loadingTimeout);
+    });
+
+    return () => clearTimeout(loadingTimeout);
+  }, []);
   return (
     <div id={id} className={`w-full ${className}`}>
       <div className="w-full flex">
@@ -68,54 +104,71 @@ const Transportations = ({ className, id }) => {
               key={data.id}
               className="max-w-[320px] resortItems flex flex-col gap-2 bg-white rounded-xl"
             >
-              <div className="rounded-xl w-full  overflow-hidden">
-                <img
-                  src={data.img}
-                  className="w-full max-h-[320px] itemImg object-cover rounded-xl"
-                />
+              <div className="rounded-xl w-full overflow-hidden">
+                {loading ? (
+                  <Skeleton width="320px" height="320px" />
+                ) : (
+                  <img
+                    src={data.img}
+                    className="w-full max-h-[320px] itemImg object-cover rounded-xl"
+                    onLoad={handleImageLoad}
+                  />
+                )}
               </div>
               <div className="w-full mt-2 mb-4 px-2 flex flex-col gap-2">
-                <p className="text-lg font-extrabold tracking-widest">
-                  {data.toda}
-                </p>
-                <div className="w-fit flex gap-1 items-center">
-                  <ImLocation className="text-[#f1be66]" />
-                  <Link to={data.loc_link} target="_blank">
-                    <Tooltip label="Location" placement="bottom">
-                      <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
-                        {data.loc}
-                      </p>
-                    </Tooltip>
-                  </Link>
-                </div>
-                <div className="w-fit flex gap-1 items-center">
-                  <SiAlwaysdata className="text-[#f1be66]" />
-                  <Tooltip label="Routes" placement="bottom">
-                    <Link to={data.rts_link} target="_blank">
-                      <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
-                        {data.rts}
-                      </p>
-                    </Link>
-                  </Tooltip>
-                </div>
-                <div className="w-fit flex gap-1 items-center">
-                  <PiHandCoinsDuotone className="text-[#f1be66]" />
-                  <Tooltip label="Fare" placement="bottom">
-                    <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
-                      ₱ {data.fare}
+                {loading ? (
+                  <>
+                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                    <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                  </>
+                ) : (
+                  <>
+                    <p className="text-lg font-extrabold tracking-widest">
+                      {data.toda}
                     </p>
-                  </Tooltip>
-                </div>
-                <div className="w-fit">
-                  <Link
-                    to={getDirection(data.direction, startingPoint)}
-                    target="_blank"
-                  >
-                    <p className="w-auto text-[#f1be66] hover:translate-x-5 duration-300 hover:font-bold">
-                      Get Direction &#8594;
-                    </p>
-                  </Link>
-                </div>
+                    <div className="w-fit flex gap-1 items-center">
+                      <ImLocation className="text-[#f1be66]" />
+                      <Link to={data.loc_link} target="_blank">
+                        <Tooltip label="Location" placement="bottom">
+                          <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
+                            {data.loc}
+                          </p>
+                        </Tooltip>
+                      </Link>
+                    </div>
+                    <div className="w-fit flex gap-1 items-center">
+                      <SiAlwaysdata className="text-[#f1be66]" />
+                      <Tooltip label="Routes" placement="bottom">
+                        <Link to={data.rts_link} target="_blank">
+                          <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
+                            {data.rts}
+                          </p>
+                        </Link>
+                      </Tooltip>
+                    </div>
+                    <div className="w-fit flex gap-1 items-center">
+                      <PiHandCoinsDuotone className="text-[#f1be66]" />
+                      <Tooltip label="Fare" placement="bottom">
+                        <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
+                          ₱ {data.fare}
+                        </p>
+                      </Tooltip>
+                    </div>
+                    <div className="w-fit">
+                      <Link
+                        to={getDirection(data.direction, startingPoint)}
+                        target="_blank"
+                      >
+                        <p className="w-auto text-[#f1be66] hover:translate-x-5 duration-300 hover:font-bold">
+                          Get Direction &#8594;
+                        </p>
+                      </Link>
+                    </div>
+                  </>
+                )}
               </div>
             </div>
           ))}
