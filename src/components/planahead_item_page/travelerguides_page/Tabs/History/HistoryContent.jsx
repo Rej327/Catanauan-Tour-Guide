8ba8050e0e1365fed6d16cb1118e7b5eeebb9aa6
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-09-01 14:04:18 +0800

    Add Skeleton Loader in Traveler Guides Page

--- src/components/planahead_item_page/travelerguides_page/Tabs/History/HistoryContent.jsx
+++ src/components/planahead_item_page/travelerguides_page/Tabs/History/HistoryContent.jsx
@@ -1,10 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import OtherContent from "./OtherContent";
 import { LangContext } from "../../../../../App";
 import { history_3, history_4 } from "../../../../../assets";
+import { Skeleton } from "@chakra-ui/react";
 
 const HistoryContent = () => {
   const { language } = useContext(LangContext);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const [showSkeletons, setShowSkeletons] = useState(true);
+
+  const images = [history_3, history_4];
+
+  useEffect(() => {
+    const imgPromises = images.map((src) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+        img.onerror = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+      });
+    });
+    Promise.all(imgPromises).then(() => {
+      setShowSkeletons(false);
+    });
+  }, [images]);
+
   return (
     <div className="w-full flex flex-wrap md:flex-nowrap gap-2 justify-center lg:justify-between ">
       <div className="max-w-[850px] flex flex-col gap-4 my-5">
@@ -17,11 +43,15 @@ const HistoryContent = () => {
         <p className="text-lg">
           {language?.plan_page?.traveler_guides?.content_1}
         </p>
-        <img
-          src={history_3}
-          alt="Catanauan Image"
-          className="w-full h-auto object-cover"
-        />
+        {showSkeletons && (
+          <Skeleton
+            width="100%"
+            height="300px"
+            startColor="gray.200"
+            endColor="gray.400"
+          />
+        )}
+        {!showSkeletons && <img src={history_4} alt="History Image" />}
       </div>
       <div className="w-[400px]">
         <OtherContent />
--- src/components/planahead_item_page/travelerguides_page/Tabs/History/OtherContent.jsx
+++ src/components/planahead_item_page/travelerguides_page/Tabs/History/OtherContent.jsx
@@ -1,21 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   history_0,
   history_1,
   history_2,
   history_4,
 } from "../../../../../assets";
+import { Skeleton } from "@chakra-ui/react";
 
 const OtherContent = () => {
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const [showSkeletons, setShowSkeletons] = useState(true);
+
+  const images = [history_0, history_1, history_2, history_4];
+
+  useEffect(() => {
+    const imgPromises = images.map((src) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+        img.onerror = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+      });
+    });
+    Promise.all(imgPromises).then(() => {
+      setShowSkeletons(false);
+    });
+  }, [images]);
+
   return (
     <div className="w-full md:w-[300px] lg:w-[400px] bg-[#FCE9CB] flex flex-col gap-4 p-6 mx-auto">
       <p className="font-bold text-lg">Where History Comes Alive!</p>
       <p className="text-lg">Something In 18th Century</p>
-      <img src={history_4} alt="History Image" />
+      {showSkeletons && (
+        <Skeleton
+          width="100%"
+          height="300px"
+          startColor="gray.200"
+          endColor="gray.400"
+        />
+      )}
+      {!showSkeletons && <img src={history_4} alt="History Image" />}
       <p className="text-lg">Catanauan Quezon Now!</p>
-      <img src={history_0} alt="History Image" />
-      <img src={history_1} alt="History Image" />
-      <img src={history_2} alt="History Image" />
+      {showSkeletons && (
+        <Skeleton
+          width="100%"
+          height="300px"
+          startColor="gray.200"
+          endColor="gray.400"
+        />
+      )}
+      {!showSkeletons && <img src={history_0} alt="History Image" />}
+      {showSkeletons && (
+        <Skeleton
+          width="100%"
+          height="300px"
+          startColor="gray.200"
+          endColor="gray.400"
+        />
+      )}
+      {!showSkeletons && <img src={history_1} alt="History Image" />}
+      {showSkeletons && (
+        <Skeleton
+          width="100%"
+          height="300px"
+          startColor="gray.200"
+          endColor="gray.400"
+        />
+      )}
+      {!showSkeletons && <img src={history_2} alt="History Image" />}
     </div>
   );
 };
