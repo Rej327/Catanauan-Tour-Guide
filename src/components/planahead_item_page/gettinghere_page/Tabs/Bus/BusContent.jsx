c46db69bdddc52e4224013bde362836aa8ce3fbd
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-09-01 14:12:50 +0800

    Add Skeleton Loader in Getting Here Page By Bus Tab

--- src/components/planahead_item_page/gettinghere_page/Tabs/Bus/BusContent.jsx
+++ src/components/planahead_item_page/gettinghere_page/Tabs/Bus/BusContent.jsx
@@ -1,35 +1,69 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { bybus } from "../../../../../assets";
 import BusOtherContent from "./BusOtherContent";
 import { LangContext } from "../../../../../App";
+import { Skeleton } from "@chakra-ui/react";
 
 const BusContent = () => {
   const { language } = useContext(LangContext);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const [showSkeletons, setShowSkeletons] = useState(true);
+
+  const images = [bybus];
+
+  useEffect(() => {
+    const imgPromises = images.map((src) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+        img.onerror = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+      });
+    });
+    Promise.all(imgPromises).then(() => {
+      setShowSkeletons(false);
+    });
+  }, [images]);
 
   return (
     <div className="w-full flex flex-wrap md:flex-nowrap gap-2 justify-center lg:justify-between ">
       <div className="max-w-[850px] flex flex-col gap-5 my-5">
         <p className="font-bold text-2xl">
-          {" "}
           {language?.plan_page?.getting_here?.by_bus?.title}
         </p>
         <p className="text-lg">
           {language?.plan_page?.getting_here?.by_bus?.content}
         </p>
-        <img
-          src={bybus}
-          alt="Catanauan Image"
-          className="w-full h-[80vh] my-5 object-contain"
-        />
+
+        {showSkeletons && (
+          <Skeleton
+            width="100%"
+            height="80vh"
+            startColor="gray.200"
+            endColor="gray.400"
+          />
+        )}
+        {!showSkeletons && (
+          <img
+            src={bybus}
+            alt="Catanauan Image"
+            className="w-full h-[80vh] my-5 object-contain"
+          />
+        )}
+
         <p className="font-bold text-2xl">
-          {" "}
           {language?.plan_page?.getting_here?.by_bus?.sub_title_0}
         </p>
         <p className="text-lg">
           {language?.plan_page?.getting_here?.by_bus?.sub_content_0}
         </p>
         <p className="font-bold text-2xl">
-          {" "}
           {language?.plan_page?.getting_here?.by_bus?.sub_title_1}
         </p>
         <p className="text-lg">
