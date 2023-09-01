8bb6c65405f8c02dd7e8010716338b4be4c91c5f
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-09-01 13:42:38 +0800

    Add Skeleton Loader in Falls Page

--- src/components/falls_page/FallsBody.jsx
+++ src/components/falls_page/FallsBody.jsx
@@ -1,76 +1,141 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { LangContext } from "../../App";
-import { falls } from "../../assets";
 import { ImLocation } from "react-icons/im";
 import { getDirection } from "../helper";
 import { Link } from "react-router-dom";
+import { Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const FallsBody = () => {
   const { language, startingPoint } = useContext(LangContext);
-  console.log(language.falls_page);
+  const [loading, setLoading] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+
+  const handleImageLoad = () => {
+    setImagesLoaded(imagesLoaded + 1);
+
+    if (imagesLoaded === language?.falls_page?.length) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 999999);
+
+    const imgPromises = language?.falls_page?.map((data) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = data.img;
+        img.onload = () => {
+          handleImageLoad();
+          resolve();
+        };
+      });
+    });
+
+    Promise.all(imgPromises).then(() => {
+      setLoading(false);
+      clearTimeout(loadingTimeout);
+    });
+
+    return () => clearTimeout(loadingTimeout);
+  }, []);
 
   return (
     <div className="max-w-[1300px] mx-auto">
-      {language.falls_page.map((data) => (
-        <div className="flex flex-wrap justify-evenly border-b-[1px] py-10 gap-5">
-          <div className="w-[400px] h-[300px] overflow-hidden">
-            <img
-              src={data.img}
-              alt={`${data.title} Image`}
-              className="w-[400px] h-[300px] object-cover hover:scale-125 duration-700 delay-75 ease-in-out"
-            />
-          </div>
-          <div className="w-[800px] px-4 h-fit my-auto flex flex-col gap-5">
-            <div className="flex flex-col gap-4">
-              <p className="text-2xl font-bold">{data.title}</p>
-
-              <p className="text-lg">{data.content}</p>
-
-              {data.contact ? (
-                <p className="text-base">
-                  Contact Person:{" "}
-                  <Link
-                    to={data.dial_number}
-                    target="_blank"
-                    className="hover:underline underline-offset-8"
-                  >
-                    {data.contact}
-                  </Link>
-                </p>
-              ) : (
-                <p className="text-base">
-                  Contact Person:
-                  <span className=" hover:cursor-not-allowed">
-                    {data.unavail}
-                  </span>
-                </p>
-              )}
-              <p className="text-base">Travel Time: {data.travel_time}</p>
-            </div>
-            <div className="w-full flex flex-col md:flex-row gap-4 justify-between">
-              <div className="w-fit flex gap-1 items-center">
-                <ImLocation className="text-[#f1be66]" />
-
-                <Link to={data.loc_link} target="_blank">
-                  <p className="text-sm hover:underline underline-offset-8 cursor-pointer">
-                    {data.location}
-                  </p>
-                </Link>
+      {loading ? (
+        language?.falls_page?.map((_, index) => (
+          <div
+            key={index}
+            className="flex flex-wrap justify-evenly border-b-[1px] py-10 gap-5"
+          >
+            <Skeleton width="400px" height="300px" />
+            <div className="w-[800px] px-4 h-fit my-auto flex flex-col gap-5">
+              <div className="flex flex-col gap-4">
+                <SkeletonText mt={2} noOfLines={1} spacing="4" />
+                <SkeletonText mt={2} noOfLines={1} spacing="4" />
+                <SkeletonText mt={2} noOfLines={1} spacing="4" />
               </div>
-              <div className="w-fit gap-4">
-                <Link
-                  to={getDirection(data.direction, startingPoint)}
-                  target="_blank"
-                >
-                  <p className="w-fit flex items-center px-4 py-2 rounded-xl bg-[#f1be66] text-white hover:scale-110 duration-200 ease-in cursor-pointer">
-                    Get Direction
-                  </p>
-                </Link>
+              <div className="w-full flex flex-col md:flex-row gap-4 justify-between">
+                <SkeletonText
+                  className="w-[15rem]"
+                  mt={2}
+                  noOfLines={1}
+                  spacing="4"
+                />
+                <Skeleton className="w-[150px] h-[50px]" />
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <>
+          {language.falls_page.map((data, index) => (
+            <div
+              key={index}
+              className="flex flex-wrap justify-evenly border-b-[1px] py-10 gap-5"
+            >
+              <div className="w-[400px] h-[300px] overflow-hidden">
+                <img
+                  src={data.img}
+                  alt={`${data.title} Image`}
+                  className="w-[400px] h-[300px] object-cover hover:scale-125 duration-700 delay-75 ease-in-out"
+                />
+              </div>
+              <div className="w-[800px] px-4 h-fit my-auto flex flex-col gap-5">
+                <div className="flex flex-col gap-4">
+                  <p className="text-2xl font-bold">{data.title}</p>
+
+                  <p className="text-lg">{data.content}</p>
+
+                  {data.contact ? (
+                    <p className="text-base">
+                      Contact Person:{" "}
+                      <Link
+                        to={data.dial_number}
+                        target="_blank"
+                        className="hover:underline underline-offset-8"
+                      >
+                        {data.contact}
+                      </Link>
+                    </p>
+                  ) : (
+                    <p className="text-base">
+                      Contact Person:
+                      <span className=" hover:cursor-not-allowed">
+                        {data.unavail}
+                      </span>
+                    </p>
+                  )}
+                  <p className="text-base">Travel Time: {data.travel_time}</p>
+                </div>
+                <div className="w-full flex flex-col md:flex-row gap-4 justify-between">
+                  <div className="w-fit flex gap-1 items-center">
+                    <ImLocation className="text-[#f1be66]" />
+
+                    <Link to={data.loc_link} target="_blank">
+                      <p className="text-sm hover:underline underline-offset-8 cursor-pointer">
+                        {data.location}
+                      </p>
+                    </Link>
+                  </div>
+                  <div className="w-fit gap-4">
+                    <Link
+                      to={getDirection(data.direction, startingPoint)}
+                      target="_blank"
+                    >
+                      <p className="w-fit flex items-center px-4 py-2 rounded-xl bg-[#f1be66] text-white hover:scale-110 duration-200 ease-in cursor-pointer">
+                        Get Direction
+                      </p>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}{" "}
+        </>
+      )}
     </div>
   );
 };
