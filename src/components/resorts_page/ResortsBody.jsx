2b4c38dd3cff617f506ee64cc1fa11ccce24a8bd
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-09-01 10:38:06 +0800

    Add Skeleton Component in Resorts Page

--- src/components/resorts_page/ResortsBody.jsx
+++ src/components/resorts_page/ResortsBody.jsx
@@ -1,10 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ImLocation } from "react-icons/im";
 import { LangContext } from "../../App";
 import { Link } from "react-router-dom";
+import { Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const ResortsBody = () => {
   const { language } = useContext(LangContext);
+  const [loading, setLoading] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+
+  const handleImageLoad = () => {
+    setImagesLoaded(imagesLoaded + 1);
+
+    if (imagesLoaded === language?.resorts_page?.length) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 999999);
+
+    const imgPromises = language?.resorts_page?.map((data) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = data.img;
+        img.onload = () => {
+          handleImageLoad();
+          resolve();
+        };
+      });
+    });
+
+    Promise.all(imgPromises).then(() => {
+      setLoading(false);
+      clearTimeout(loadingTimeout);
+    });
+
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
   return (
     <div className="max-w-[1300px] pb-10 mx-auto justify-center items-center flex flex-wrap gap-10 px-2">
       {language?.resorts_page?.map((data) => (
@@ -13,25 +49,41 @@ const ResortsBody = () => {
           className="relative max-w-[290px] h-[410px] resortItems flex flex-col gap-2 bg-white rounded-xl"
         >
           <div className="rounded-xl w-[290px] h-[250px] overflow-hidden">
-            <img
-              src={data.img}
-              className="w-[290px] h-[250px] itemImg  object-cover rounded-xl"
-            />
+            {loading ? (
+              <Skeleton width="100%" height="250px" />
+            ) : (
+              <img
+                src={data.img}
+                className="w-[290px] h-[250px] itemImg  object-cover rounded-xl"
+                onLoad={handleImageLoad}
+              />
+            )}
           </div>
           <div className="w-full mt-2 mb-4 px-2 flex flex-col gap-2">
-            <p className="w-fit text-xs text-[#f1be66] font-bold">
-              {data.category}
-            </p>
-            <p className="text-lg font-extrabold">{data.title}</p>
-            <div className="w-fit flex gap-1 items-center">
-              <ImLocation className="text-[#f1be66]" />
-              <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
-                {data.location}
-              </p>
-            </div>
-            <p className="absolute bottom-5 left-[45%] text-sm hover:text-[#be8624] underline duration-200 cursor-pointer hover:no-underline underline-offset-8">
-              <Link to={`/resorts/${data.id}`}>{data.btn_txt}</Link>
-            </p>
+            {loading ? (
+              <>
+                <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                <SkeletonText mt="4" noOfLines={1} spacing="4" />
+                <SkeletonText mt="4" noOfLines={1} spacing="4" />
+              </>
+            ) : (
+              <>
+                <p className="w-fit text-xs text-[#f1be66] font-bold">
+                  {data.category}
+                </p>
+                <p className="text-lg font-extrabold">{data.title}</p>
+                <div className="w-fit flex gap-1 items-center">
+                  <ImLocation className="text-[#f1be66]" />
+                  <p className="text-sm hover:text-[#be8624] duration-200 cursor-pointer">
+                    {data.location}
+                  </p>
+                </div>
+                <p className="absolute bottom-5 left-[45%] text-sm hover:text-[#be8624] underline duration-200 cursor-pointer hover:no-underline underline-offset-8">
+                  <Link to={`/resorts/${data.id}`}>{data.btn_txt}</Link>
+                </p>
+              </>
+            )}
           </div>
         </div>
       ))}
