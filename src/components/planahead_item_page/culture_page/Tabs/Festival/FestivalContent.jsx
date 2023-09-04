6e0d0cabe913c9f3f80c9e81fffa35db4d93f55b
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-09-04 11:52:59 +0800

    Add Skeleton Loader in Culture Page

--- src/components/planahead_item_page/culture_page/Tabs/Festival/FestivalContent.jsx
+++ src/components/planahead_item_page/culture_page/Tabs/Festival/FestivalContent.jsx
@@ -1,10 +1,36 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Skeleton } from "@chakra-ui/react";
 import OtherContent from "./OtherContent";
 import { festival_3 } from "../../../../../assets";
 import { LangContext } from "../../../../../App";
 
 const FestivalContent = () => {
   const { language } = useContext(LangContext);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const [showSkeletons, setShowSkeletons] = useState(true);
+
+  const images = [festival_3];
+
+  useEffect(() => {
+    const imgPromises = images.map((src) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+        img.onerror = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+      });
+    });
+    Promise.all(imgPromises).then(() => {
+      setShowSkeletons(false);
+    });
+  }, [images]);
+
   return (
     <div className="w-full flex flex-wrap md:flex-nowrap gap-2 justify-center lg:justify-between ">
       <div className="max-w-[850px] flex flex-col gap-4 my-5">
@@ -20,11 +46,16 @@ const FestivalContent = () => {
         <p className="text-lg">
           {language?.plan_page?.cultures?.festival?.content_2}
         </p>
-        <img
-          src={festival_3}
-          alt="Catanauan Image"
-          className="w-full h-[18rem] object-cover"
-        />
+        {showSkeletons && (
+          <Skeleton variant="image" width="100%" height="18rem" />
+        )}
+        {!showSkeletons && (
+          <img
+            src={festival_3}
+            alt="Catanauan Image"
+            className="w-full h-[18rem] object-cover"
+          />
+        )}
       </div>
       <div className="w-[400px]">
         <OtherContent />
--- src/components/planahead_item_page/culture_page/Tabs/Festival/OtherContent.jsx
+++ src/components/planahead_item_page/culture_page/Tabs/Festival/OtherContent.jsx
@@ -1,24 +1,71 @@
-import React, { useContext } from "react";
-import { festival_0, festival_1, festival_2 } from "../../../../../assets";
+import React, { useState, useEffect, useContext } from "react";
+import { Skeleton } from "@chakra-ui/react";
 import { LangContext } from "../../../../../App";
 
 const OtherContent = () => {
   const { language } = useContext(LangContext);
-  console.log(language?.cultures);
+
+  const [loading, setLoading] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+
+  const handleImageLoad = () => {
+    setImagesLoaded(imagesLoaded + 1);
+
+    if (
+      imagesLoaded ===
+      language?.plan_page?.cultures?.festival?.festival_img?.length
+    ) {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const loadingTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 999999);
+
+    const imgPromises =
+      language?.plan_page?.cultures?.festival?.festival_img?.map((data) => {
+        return new Promise((resolve) => {
+          const img = new Image();
+          img.src = data.img;
+          img.onload = () => {
+            handleImageLoad();
+            resolve();
+          };
+        });
+      });
+
+    Promise.all(imgPromises).then(() => {
+      setLoading(false);
+      clearTimeout(loadingTimeout);
+    });
+
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
   return (
     <div className="w-full md:w-[300px] lg:w-[400px] bg-[#FCE9CB] flex flex-col gap-4 p-6 mx-auto">
       <p className="font-bold">Boling - Boling Festival Parade</p>
-      {language?.plan_page?.cultures?.festival?.festival_img
-        ?.slice(0, 3)
-        .map((data) => (
-          <div className="w-full">
-            <img
-              src={data.img}
-              alt="Boling-Boling Festival Image"
-              className="h-[300px] w-full object-cover"
-            />
-          </div>
-        ))}
+      {loading && (
+        <div className="w-full flex flex-col gap-4">
+          <Skeleton variant="image" width="100%" height="300px" />
+          <Skeleton variant="image" width="100%" height="300px" />
+          <Skeleton variant="image" width="100%" height="300px" />
+        </div>
+      )}
+      {!loading &&
+        language?.plan_page?.cultures?.festival?.festival_img
+          ?.slice(0, 3)
+          .map((data) => (
+            <div className="w-full">
+              <img
+                src={data.img}
+                alt="Boling-Boling Festival Image"
+                className="h-[300px] w-full object-cover"
+              />
+            </div>
+          ))}
     </div>
   );
 };
