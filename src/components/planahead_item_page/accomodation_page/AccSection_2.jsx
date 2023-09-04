630c39c67f7a8fc250fa12d59cef425d0e1b24cd
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-09-04 11:58:28 +0800

    Add Skeleton Loader in Accomodation Page

--- src/components/planahead_item_page/accomodation_page/AccSection_2.jsx
+++ src/components/planahead_item_page/accomodation_page/AccSection_2.jsx
@@ -1,10 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { hc2 } from "../../../assets";
 import { LangContext } from "../../../App";
+import { Skeleton } from "@chakra-ui/react";
 
 const AccSection_2 = ({ className }) => {
   const { language } = useContext(LangContext);
+
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const [showSkeletons, setShowSkeletons] = useState(true);
+
+  const images = [hc2];
+
+  useEffect(() => {
+    const imgPromises = images.map((src) => {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+        img.onerror = () => {
+          setImagesLoaded((prev) => prev + 1);
+          resolve();
+        };
+      });
+    });
+    Promise.all(imgPromises).then(() => {
+      setShowSkeletons(false);
+    });
+  }, [images]);
+
   return (
     <div className={`w-full bg-[#FEFEFF] ${className}`}>
       <div className="max-w-[1300px] flex flex-col mx-auto">
@@ -21,11 +48,16 @@ const AccSection_2 = ({ className }) => {
           <p className="text-lg">
             {language.plan_page?.accomodation?.sub_content_0}
           </p>
-          <img
-            src={hc2}
-            alt="Accomodation Image"
-            className="h-[70vh] object-cover"
-          />
+          {showSkeletons && (
+            <Skeleton variant="image" width="100%" height="70vh" />
+          )}
+          {!showSkeletons && (
+            <img
+              src={hc2}
+              alt="Accomodation Image"
+              className="h-[70vh] object-cover"
+            />
+          )}
 
           <p className="text-[#d4a148] w-fit pl-2 text-lg hover:translate-x-2 hover:font-bold duration-300 ">
             <Link to="/hotels">
