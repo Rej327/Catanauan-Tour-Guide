18c8da96f88604399cae6f7653587ae1dda2e27b
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-09-14 12:06:27 +0800

    Add Gallery Modal Component for Getting Around Items

---
+++ src/components/planahead_item_page/gettingarround_page/GalleryModal.jsx
@@ -0,0 +1,48 @@
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+import WSPGallery from "../../common/WSPGallery";
+
+const GalleryModal = ({ dataInfo }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <p
+        className="w-auto text-[#f1be66] text-center hover:text-[#be8624] cursor-pointer"
+        onClick={onOpen}
+      >
+        Gallery &#8594;
+      </p>
+
+      <Modal size="full" isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <div className="max-w-[1300px] mx-auto">
+              <p className="text-2xl tracking-widest">Gallery</p>
+            </div>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div className="w-full py-2 border-b-2">
+              <p className="text-2xl tracking-widest mt-5 mb-2"> - Place</p>
+              <WSPGallery setData={dataInfo} />
+            </div>
+          </ModalBody>
+
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default GalleryModal;
