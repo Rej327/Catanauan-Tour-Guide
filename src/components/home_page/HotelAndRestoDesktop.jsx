6a1fce1a42082fbe5b7f84aa84ab9a58e1c042ba
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-09-19 14:59:14 +0800

    Change design of hotel and resto section in home page

--- src/components/home_page/HotelAndRestoDesktop.jsx
+++ src/components/home_page/HotelAndRestoDesktop.jsx
@@ -1,43 +1,110 @@
-import React, { useContext } from "react";
-import Slider from "react-slick";
-import HotelAndRestoCarouselItem from "./HotelAndRestoCarouselItem";
-import SlideArrow from "../common/SlideArrow";
+import React, { useState, useEffect, useContext } from "react";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
+import { Link, useNavigate } from "react-router-dom";
 import { LangContext } from "../../App";
+import Button from "../common/Button";
+import SlideArrow from "../common/SlideArrow";
 
-const HotelAndRestoDesktop = ({ className, id }) => {
+const HotelAndRestoDesktop = () => {
   const { language } = useContext(LangContext);
-  const settings = {
-    infinite: true,
-    speed: 1500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    pauseOnHover: true,
+  const [currentSection, setCurrentSection] = useState(1);
+  const router = useNavigate();
+  const sectionRef = useRef();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        sectionRef.current &&
+        window.scrollY + window.innerHeight >=
+          sectionRef.current.offsetTop + sectionRef.current.clientHeight
+      ) {
+        setCurrentSection(currentSection + 1);
+
+        if (currentSection === 3) {
+          router.push("/next-section");
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [sectionRef, currentSection, router]);
+
+  const ScrollableSection = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
+    return (
+      <div
+        ref={sectionRef}
+        className={`ScrollableSection bg-[url(./assets/HotelAndResto/hotelandresto.jpg)] bg-fixed w-auto bg-cover bg-center h-screen  overflow-y-scroll ${
+          isMobile ? "md:overflow-y-hidden" : ""
+        }`}
+      >
+        <div className="bg-black bg-opacity-60 w-full h-screen">{children}</div>
+      </div>
+    );
   };
 
   return (
-    <Slider className={`overflow-hidden ${className}`} {...settings}>
-      <div
-        id={id}
-        className="bg-[url(./assets/HotelAndResto/hotelandresto.jpg)] w-auto bg-cover bg-center h-screen"
-      >
-        <div className="relative bg-black bg-opacity-60 w-full h-screen flex items-center flex-col justify-center">
-          <p className="font-title max-w-[700px] px-2 text-center text-[#ececec] text-4xl tracking-wide">
-            {language?.hotel_resto_section?.cover}
-          </p>
-          <div className="absolute bottom-[5rem] flex max-w-[300px] opacity-50 items-center justify-center gap-10">
-            <SlideArrow />
-            <p className="text-[#ececec] text-sm tracking-wider ">
-              {language?.hotel_resto_section?.swipe_txt}
+    <div className="mt-10 fixed-background-section ">
+      <ScrollableSection>
+        <div className="h-screen w-full flex justify-center items-center">
+          <div className="relative h-screen flex items-center flex-col justify-center">
+            <p className="font-title max-w-[700px] px-2 text-center text-[#ececec] text-4xl tracking-wide">
+              {language?.hotel_resto_section?.cover}
             </p>
-            <SlideArrow className="-mt-[.4rem] -rotate-90" />
           </div>
         </div>
-      </div>
-      <div className="h-screen overflow-hidden">
-        <HotelAndRestoCarouselItem />
-      </div>
-    </Slider>
+      </ScrollableSection>
+      <ScrollableSection>
+        <div
+          id="hotel"
+          className="h-screen w-full flex justify-center items-center"
+        >
+          <div
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-center"
+            data-aos-duration="500"
+            className="mx-4 relative border-[1px] p-10 flex items-center flex-col justify-center"
+          >
+            <p className="font-title max-w-[700px] px-2 text-center text-[#ececec] text-3xl tracking-wide">
+              {language?.hotel_resto_section?.hotel_content}
+            </p>
+            <Link to="/hotels">
+              <p className="mt-10 text-xl text-[#ececec] underline underline-offset-8 hover:no-underline">
+                Go to Hotels
+              </p>
+            </Link>
+          </div>
+        </div>
+      </ScrollableSection>
+      <ScrollableSection>
+        <div
+          id="resto"
+          className="h-screen w-full flex justify-center items-center"
+        >
+          <div
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-center"
+            data-aos-duration="500"
+            className="mx-4 relative border-[1px] p-10 flex items-center flex-col justify-center"
+          >
+            <p className="font-title max-w-[700px] px-2 text-center text-[#ececec] text-3xl tracking-wide">
+              {language?.hotel_resto_section?.resto_content}
+            </p>
+            <Link to="/restaurants">
+              <p className="mt-10 text-xl text-[#ececec] underline underline-offset-8 hover:no-underline">
+                Go to Restaurants
+              </p>
+            </Link>
+          </div>
+        </div>
+      </ScrollableSection>
+    </div>
   );
 };
 
