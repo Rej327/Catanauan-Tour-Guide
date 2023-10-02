f5c5b43c1c9a17ff4c4d4e73c6c82f5670d598ac
JEFFERSON_RESURRECCION <resujeff27@gmail.com>
2023-10-02 09:20:28 +0800

    Shutdown Tempo

--- src/components/Under.jsx
+++ src/components/Under.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Under = () => {
   return (
     <div className="bg-[url(./assets/under.jpg)] w-full h-screen flex flex-col gap-10 items-center justify-center ">
-      <div className="w-full h-screen bg-black bg-opacity-50 flex flex-col gap-10 items-center justify-center">
+      <div className="p-4 w-full h-screen bg-black bg-opacity-50 flex flex-col gap-10 items-center justify-center">
         <p className="font-header text-4xl text-white">DiscoverCatanauan</p>
         <p className="text-4xl text-white">Under Construction!</p>
         <p className="text-2xl text-white">
