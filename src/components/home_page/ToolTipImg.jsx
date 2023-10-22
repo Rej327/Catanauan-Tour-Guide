b1e1c6dd741878c0e4c76e9543abd70410b1d5a2
Rej327 <resujeff27@gmail.com>
2023-10-22 16:54:14 +0800

    Add Skeleton Loading (Test)

--- src/components/home_page/ToolTipImg.jsx
+++ src/components/home_page/ToolTipImg.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Skeleton } from "@chakra-ui/react";
 
 const ToolTipImg = ({ tipImg }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <div className="rounded-xl w-[400px] mx-4 shadow-2xl border-b-2 border-[#F9DBA9]">
-      <img
-        src={tipImg}
-        className="rounded-xl w-[400px] h-[350px] shadow-2xl object-cover"
-      />
+      <Skeleton
+        rounded="2xl"
+        isLoaded={imageLoaded}
+        startColor="gray.200"
+        endColor="gray.400"
+        height="350px"
+      >
+        <img
+          src={tipImg}
+          className="rounded-xl w-[400px] h-[350px] shadow-2xl object-cover"
+          onLoad={handleImageLoad}
+        />
+      </Skeleton>
     </div>
   );
 };
