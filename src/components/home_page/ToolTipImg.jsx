7db20df4cea6e9f940926c11c6fe60ad4256a962
Rej327 <resujeff27@gmail.com>
2023-10-22 16:11:40 +0800

    Remove Margin

--- src/components/home_page/ToolTipImg.jsx
+++ src/components/home_page/ToolTipImg.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ToolTipImg = ({ tipImg }) => {
   return (
-    <div className="rounded-xl w-[400px] mx-4 shadow-2xl border-b-2 border-[#F9DBA9]">
+    <div className="rounded-xl w-[400px] shadow-2xl border-b-2 border-[#F9DBA9]">
       <img
         src={tipImg}
         className="rounded-xl w-[400px] h-[350px] shadow-2xl object-cover"
